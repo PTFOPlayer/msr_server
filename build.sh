0e5c3d0cba0fd908e214ce8281aa18f43314a47e
PTFOPlayer <patryk041103@gmail.com>
2023-03-03 22:08:12 +0100

    added build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+mkdir build 
+cd build
+cmake .. 
+make
+cd ..
+
+sudo cp ./msr_gen /usr/bin/
+if (sudo cp ../msr_server.service /etc/systemd/system/) || (sudo cp ./msr_server.service /etc/systemd/system/)
+then
+    echo "succes, build ended"
+fi

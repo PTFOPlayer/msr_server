831b120bb1107741f41d2f585ff0e0a8f1e6d812
PTFOPlayer <patryk041103@gmail.com>
2023-03-04 22:36:50 +0100

    temperature hot fix

--- src/data_getters/msr_data.hpp
+++ src/data_getters/msr_data.hpp
@@ -91,11 +91,7 @@ double get_cpu_temperature(int fd)
 	result = read_msr(fd, MSR_TEMPERATURE_TARGET);
 	double t2 = (double)((result >> 16) & ((1 << 7) - 1));
 
-	result = read_msr(fd, MSR_TEMPERATURE_TARGET);
-	double t3 = (double)((result >> 8) & ((1 << 7) - 1));
-	//cout << t3 << endl;
-
-	return t2 - t1 - t3;
+	return t2 - t1;
 }
 
 bool get_cpu_ht(int fd)

7a6de8937acb59c68bd42567a99509094402c4eb
PTFOPlayer <patryktofil03@gmail.com>
2023-03-18 12:30:17 +0100

    REST support update

--- build.sh
+++ build.sh
@@ -1,11 +1,51 @@
+#!/bin/bash
 mkdir build 
 cd build
 cmake .. 
 make
 cd ..
 
+if (which npm) && (which npx)
+then
+    echo "npm, npx: OK"
+else
+    
+    echo "npm and/or npx not fount"
+    exit 1
+fi 
+
+if (which node)
+then
+    echo "node: OK"
+else
+    
+    echo "node not found"
+    exit 1
+fi 
+
+if (which g++) && (which cmake) && (which make)
+then
+    echo "g++, cmake, make: OK"
+else
+    
+    echo "g++, cmake or make not found"
+    exit 1
+fi
+
+sudo npm install ts-node -g
+sudo npm install 
+
+sudo mkdir /var/msr_server
+sudo cp ./src/msr_server.ts /var/msr_server/
 sudo cp ./build/msr_gen /usr/bin/
-if (sudo cp ../msr_server.service /etc/systemd/system/) || (sudo cp ./msr_server.service /etc/systemd/system/)
+if (sudo cp ./msr_server.service /etc/systemd/system/) && (sudo cp ./msr_web_server.service /etc/systemd/system/)
 then
     echo "succes, build ended"
+else 
+    if (sudo cp ../msr_server.service /etc/systemd/system/) && (sudo cp ../msr_web_server.service /etc/systemd/system/)
+    then 
+        echo "succes, build ended"
+    else 
+        echo "error building"
+    fi
 fi
---
+++ src/msr_server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import fs from "fs";
+import { exec } from 'child_process';
+
+const app = express();
+
+app.get('/', async (req: any, res: { send: (arg0: any) => void; }) => {
+    await exec('sudo msr_gen -o', (error, stdout, stderr) => {
+        if( stdout !== null) return res.send(stdout);
+        else if ( stderr !== null) res.send(stderr)
+        else return res.send(error);
+    });
+});
+
+app.listen(3230, () => console.log('listening on port 3230'));

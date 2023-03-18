70a614094c00834e90aef230546e50c7413dbd70
PTFOPlayer <patryk041103@gmail.com>
2023-03-18 12:30:17 +0100

    REST support update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <fstream>
 #include <iostream>
 #include <math.h>
-#include <thread>
 
 #include "data_getters/cpuid_data.hpp"
 #include "data_getters/msr_data.hpp"
@@ -11,11 +10,6 @@
 #define TIME_MUL 10
 
 using namespace std;
-
-void start_node() {
-	cout << "server starting..." << endl;
-	system("node ./src/msr_server.js &");
-}
 
 int main(int argc, char const *argv[])
 {
@@ -28,56 +22,93 @@ int main(int argc, char const *argv[])
 	string filepath = "/msr_data.toml";
 	ofstream file;
 
-	if (argc >= 2) {
-		if (strcmp(argv[1], "-w") == 0) {
-			thread t(start_node);
-			t.join();
-		}
-	}
-
-	cout << "starting data parsers..." << endl;
-	
-	while (true)
+	if (argc >= 2)
 	{
-		result = msr_data::read_msr(fd, MSR_POWER_UNIT);
-		cpu_energy_units = pow(0.5, (double)((result >> 8) & 0x1f));
+		if (strcmp(argv[1], "-f") == 0)
+		{
+			while (true)
+			{
+				result = msr_data::read_msr(fd, MSR_POWER_UNIT);
+				cpu_energy_units = pow(0.5, (double)((result >> 8) & 0x1f));
 
-		package_before = msr_data::get_package_power_before(fd, cpu_energy_units);
-		msleep(1000 / TIME_MUL);
-		package_after = msr_data::get_package_power_after(fd, cpu_energy_units);
+				package_before = msr_data::get_package_power_before(fd, cpu_energy_units);
+				msleep(1000 / TIME_MUL);
+				package_after = msr_data::get_package_power_after(fd, cpu_energy_units);
 
-		package_power = (package_after - package_before) * TIME_MUL;
+				package_power = (package_after - package_before) * TIME_MUL;
 
-		string vendor = cpuid_data::get_cpu_vendor();
-		string name = cpuid_data::get_cpu_name();
-		double voltage = msr_data::get_cpu_voltage(fd);
-		double usage = get_cpu_usage();
-		double temperature = get_cpu_temperature_non_msr();
-		bool ht = msr_data::get_cpu_ht(fd);
-		auto cores_thread = cpuid_data::get_cpu_cores();
-		
-		file.open(filepath);
-		file << "[cpu]"
-			 << "\n"
-			 << "vendor = \""
-			 << vendor << "\"\n"
-			 << "name = \""
-			 << name << "\"\n"
-			 << "power = "
-			 << package_power << "\n"
-			 << "voltage = "
-			 << voltage << "\n"
-			 << "usage = "
-			 << usage << "\n"
-			 << "temperature = "
-			 << temperature << "\n"
-			 << "hyper_threading = "
-			 << ht << "\n"
-			 << "logical_cores = "
-			 << cores_thread.logical << "\n"
-			 << "physical_cores = "
-			 << cores_thread.physical << "\n";
+				string vendor = cpuid_data::get_cpu_vendor();
+				string name = cpuid_data::get_cpu_name();
+				double voltage = msr_data::get_cpu_voltage(fd);
+				double usage = get_cpu_usage();
+				double temperature = get_cpu_temperature_non_msr();
+				bool ht = msr_data::get_cpu_ht(fd);
+				auto cores_thread = cpuid_data::get_cpu_cores();
 
-		file.close();
+				file.open(filepath);
+				file << "[cpu]"
+					 << "\n"
+					 << "vendor = \""
+					 << vendor << "\"\n"
+					 << "name = \""
+					 << name << "\"\n"
+					 << "power = "
+					 << package_power << "\n"
+					 << "voltage = "
+					 << voltage << "\n"
+					 << "usage = "
+					 << usage << "\n"
+					 << "temperature = "
+					 << temperature << "\n"
+					 << "hyper_threading = "
+					 << ht << "\n"
+					 << "logical_cores = "
+					 << cores_thread.logical << "\n"
+					 << "physical_cores = "
+					 << cores_thread.physical << "\n";
+
+				file.close();
+			}
+		}
+		if (strcmp(argv[1], "-o") == 0)
+		{
+				result = msr_data::read_msr(fd, MSR_POWER_UNIT);
+				cpu_energy_units = pow(0.5, (double)((result >> 8) & 0x1f));
+
+				package_before = msr_data::get_package_power_before(fd, cpu_energy_units);
+				msleep(1000 / TIME_MUL);
+				package_after = msr_data::get_package_power_after(fd, cpu_energy_units);
+
+				package_power = (package_after - package_before) * TIME_MUL;
+
+				string vendor = cpuid_data::get_cpu_vendor();
+				string name = cpuid_data::get_cpu_name();
+				double voltage = msr_data::get_cpu_voltage(fd);
+				double usage = get_cpu_usage();
+				double temperature = get_cpu_temperature_non_msr();
+				bool ht = msr_data::get_cpu_ht(fd);
+				auto cores_thread = cpuid_data::get_cpu_cores();
+
+				cout << "[cpu]"
+					 << "\n"
+					 << "vendor = \""
+					 << vendor << "\"\n"
+					 << "name = \""
+					 << name << "\"\n"
+					 << "power = "
+					 << package_power << "\n"
+					 << "voltage = "
+					 << voltage << "\n"
+					 << "usage = "
+					 << usage << "\n"
+					 << "temperature = "
+					 << temperature << "\n"
+					 << "hyper_threading = "
+					 << ht << "\n"
+					 << "logical_cores = "
+					 << cores_thread.logical << "\n"
+					 << "physical_cores = "
+					 << cores_thread.physical << "\n";
+		}
 	}
 }

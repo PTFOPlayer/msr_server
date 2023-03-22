d9bff95382603823a8dd4e998b6fe14e712ac142
PTFOPlayer <patryktofil03@gmail.com>
2023-03-22 09:28:32 +0100

    better json request, fixes in execute

--- src/msr_server.ts
+++ src/msr_server.ts
@@ -1,23 +1,17 @@
 import express from "express";
-import { ExecException, exec } from 'child_process';
+import { exec, ExecException } from 'child_process';
 //const express = require("express")
 
 const app = express();
 const port = 3230;
 app.use(express.json())
 
-app.get('/', (req:any, res: { send: (arg0: any) => void; }) => {
-    exec('sudo msr_gen -o', (error, stdout, stderr) => {
-        if (stdout !== null)
-            return res.send(stdout);
-        else if (stderr !== null)
-            res.send(stderr);
-        else
-            return res.send(error);
-    });
-});
+type Res = {
+    write: (arg0: string) => void, 
+    send: (arg0: string | null | ExecException | void) => void
+}
 
-type msr_obj = {
+interface msr_obj {
     cpu: {
         vendor: string,
         name: string,
@@ -31,16 +25,33 @@ type msr_obj = {
     }
 }
 
-app.get('/json/*', (req, res) => {
-    exec('sudo msr_gen -j', (error, stdout, stderr) => {
+interface exec_json {
+    key: string,
+    command: string
+}
+
+app.get('/', (_req, res: Res) => {
+    exec('sudo msr_gen -o', (error: ExecException | null, stdout: string, stderr: string) => {
+        if (stdout !== null)
+            return res.send(stdout);
+        else if (stderr !== null)
+            res.send(stderr);
+        else
+            return res.send(error);
+    });
+});
+
+app.get('/json/*', (req: {url: string}, res: Res) => {
+    exec('sudo msr_gen -j', (error: ExecException | null, stdout: string, stderr: string) => {
         let obj: msr_obj = JSON.parse(stdout)
         if (stdout !== null) {
-            if (req.url === "/json" || req.url === "/json/")
-                res.write(stdout);
-            if (req.url.includes('/usage')) res.write(obj.cpu.usage.toString()+" ");
-            if (req.url.includes('/power')) res.write(obj.cpu.power.toString()+" ");
-            if (req.url.includes('/temperature')) res.write(obj.cpu.temperature.toString()+" ");
-            if (req.url.includes('/voltage')) res.write(obj.cpu.voltage.toString()+" ");
+            if (req.url === "/json" || req.url === "/json/") res.write(stdout);
+            else if (req.url.match('/json/cpu\*') ){
+                if (req.url.includes('/usage')) res.write(obj.cpu.usage.toString()+" ");
+                if (req.url.includes('/power')) res.write(obj.cpu.power.toString()+" ");
+                if (req.url.includes('/temperature')) res.write(obj.cpu.temperature.toString()+" ");
+                if (req.url.includes('/voltage')) res.write(obj.cpu.voltage.toString()+" ");
+            }
             return res.send()
         }
         else if (stderr !== null)
@@ -51,20 +62,19 @@ app.get('/json/*', (req, res) => {
 });
 
 const execute_middleware =(req: { body: string; }): string => {
-    let json = JSON.parse(JSON.stringify(req.body))
+    let json: exec_json = JSON.parse(JSON.stringify(req.body))
     if (json.key !== null && process.env.MSR_KEY !== null) {
         if (json.command !== null && process.env.MSR_KEY === json.key)
             return json.command
         else {
-            return "echo \"no key supplied in attached json\""
+            return "echo \"no command supplied in attached json or wrong key\""
         }
     } else {
-        return "echo \"no key supplied in attached json\""
+        return "echo \"no key supplied in attached json or system variable\""
     }
-
 }
 
-app.post('/execute', (req , res: { send: (arg0: any) => void; } ) => {
+app.post('/execute', (req: { body: string }, res: Res) => {
     exec(execute_middleware(req), (error, stdout, stderr) => {
         if (stdout !== null)
             return res.send(stdout);

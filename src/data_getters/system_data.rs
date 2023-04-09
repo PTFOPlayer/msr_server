8ac4aa76a283cdcb9ac1893cd173ed13446ad289
PTFOPlayer <patryktofil03@gmail.com>
2023-04-09 22:04:51 +0200

    huge improvement in performance

--- src/data_getters/system_data.rs
+++ src/data_getters/system_data.rs
@@ -1,13 +1,21 @@
 use sysinfo::*;
 
+use crate::cpuid_data::CPUID;
+
 #[repr(C)]
 pub struct core_stat{
     freq: u64,
-    util: f64
+    util: f64,
+    threads: i32,
+    cores: i32,
+    temperature:f32,
+    mem_total: u64,
+    mem_free: u64,
+    mem_used: u64,
 }
 
 #[no_mangle]
-pub extern "C" fn get_cpu_utils_rs(time_mul: i32) -> core_stat {
+pub extern "C" fn get_sys_utils_rs(time_mul: i32) -> core_stat {
     let mut sys = System::new_all();
     
     sys.refresh_all();
@@ -16,30 +24,16 @@ pub extern "C" fn get_cpu_utils_rs(time_mul: i32) -> core_stat {
     
     sys.refresh_all();
     
-    let res = core_stat {
-        freq: sys.global_cpu_info().frequency(),
-        util: sys.global_cpu_info().cpu_usage() as f64
-        
+    let t = sys.cpus().len() as i32;
+    let ht = match CPUID.get_feature_info() {
+        Some(val) => val.has_htt(),
+        None => false,
     };
-    
-    return res
-}
 
-#[no_mangle]
-pub extern "C" fn get_cpu_threads_rs() -> i32 {
-    let mut sys = System::new_all();
-    
-    sys.refresh_all();
-
-    sys.cpus().len() as i32
-}
-
-#[no_mangle]
-pub extern "C" fn get_cpu_temp_rs() -> f32 {
-    
-    let mut sys = System::new_all();
-    
-    sys.refresh_all();
+    let mut c = t;
+    if ht {
+        c = c/2;
+    }
 
     let mut s_temp = 0.;
     let mut i_i = 0;
@@ -51,35 +45,20 @@ pub extern "C" fn get_cpu_temp_rs() -> f32 {
     }
 
     if i_i == 0 {
-        return -1.0;
+        i_i = 1;
+        s_temp = 1.;
     }
-    return s_temp / i_i as f32;
+
+    let res = core_stat {
+        freq: sys.global_cpu_info().frequency(),
+        util: sys.global_cpu_info().cpu_usage() as f64,
+        threads: t,
+        cores: c,
+        temperature: s_temp / i_i as f32,
+        mem_total: sys.total_memory(),
+        mem_free: sys.free_memory(),
+        mem_used: sys.used_memory(),
+    };
     
-}
-
-#[no_mangle]
-pub extern "C" fn get_mem_total() -> u64 {
-    let mut sys = System::new_all();
-    
-    sys.refresh_all();
-
-    return sys.total_memory();   
-}
-
-#[no_mangle]
-pub extern "C" fn get_mem_free() -> u64 {
-    let mut sys = System::new_all();
-    
-    sys.refresh_all();
-
-    return sys.free_memory();   
-}
-
-#[no_mangle]
-pub extern "C" fn get_mem_used() -> u64 {
-    let mut sys = System::new_all();
-    
-    sys.refresh_all();
-
-    return sys.used_memory();   
+    return res
 }

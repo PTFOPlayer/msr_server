84bf0ce8f1b31554085bf3abad394026bfca1dbd
PTFOPlayer <patryk041103@gmail.com>
2023-07-25 12:02:27 +0200

    moved printing to rust and some minor fixes

--- src/lib.rs
+++ src/lib.rs
@@ -9,15 +9,26 @@ struct DataToJson {
 }
 
 #[no_mangle]
-pub extern "C" fn print_json_rs(voltage: f64, package_power: f64, time_mul: i32) {
-
+pub extern "C" fn print_json_rs(voltage: *mut f64, package_power: *mut f64, time_mul: i32) {
     let (vendor, name) = non_c_name_and_vendor();
-
     let cs = non_c_sys_utils(time_mul);
-
-    let (cpu, mem) = cs.split(voltage, package_power, vendor, name);
+    let (cpu, mem) = unsafe { cs.split(*voltage , *package_power, vendor, name) };
 
     if let Ok(serialized) = serde_json::to_string(&DataToJson {
+        cpu,
+        memory: mem
+    }) {
+        println!("{}", serialized);
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn print_toml_rs(voltage: *mut f64, package_power: *mut f64, time_mul: i32) {
+    let (vendor, name) = non_c_name_and_vendor();
+    let cs = non_c_sys_utils(time_mul);
+    let (cpu, mem) = unsafe { cs.split(*voltage , *package_power, vendor, name) };
+
+    if let Ok(serialized) = toml::to_string(&DataToJson {
         cpu,
         memory: mem
     }) {
--- src/main.c
+++ src/main.c
@@ -45,23 +45,14 @@ int main(int argc, char const *argv[])
 			// sleep depends on that
 			double package_power = get_cpu_power(fd, TIME_MUL);
 			struct CoreStat cs = get_sys_utils_rs(TIME_MUL);
-
-			long frequency = cs.freq;
-			double usage = cs.util;
-			unsigned long long memory_total = cs.mem_total;
-			unsigned long long memory_free = cs.mem_free;
-			unsigned long long memory_used = cs.mem_used;
-			float temperature = cs.temperature;
-			int threads = cs.threads;
-			int cores = cs.cores;
-			printf("[cpu]\nvendor = \"%s\"\nname = \"%s\"\npower = %lf\nvoltage = %lf\ntemperature = %f\nfrequency = %ld\nusage = %lf\nlogical_cores = %d\nphysical_cores = %d\n[memory]\ntotal = %llu\navailable = %llu\nused = %llu\n", vendor, name, package_power, voltage, temperature, frequency, usage, threads, cores, memory_total / 1024 / 1024, memory_free / 1024 / 1024, memory_used / 1024 / 1024);
+			print_toml_rs(&voltage, &package_power, TIME_MUL);
 		}
 		if (strcmp(argv[1], "-j") == 0)
 		{
 			double voltage = get_cpu_voltage(fd);
 			// sleep depends on that
 			double package_power = get_cpu_power(fd, TIME_MUL);
-			print_json_rs(voltage, package_power, TIME_MUL);
+			print_json_rs(&voltage, &package_power, TIME_MUL);
 		}
 	}
 	else

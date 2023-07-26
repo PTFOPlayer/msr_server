4bbcab22fd767e9dba30ca9e91fb0bc5b9b9ebfb
PTFOPlayer <patryk041103@gmail.com>
2023-07-27 00:07:58 +0200

    new install file, removed node part

--- build.sh
+++ build.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-if (which npm) && (which npx)
-then
-    echo "npm, npx: OK"
-else
-    
-    echo "npm and/or npx not fount"
-    exit 1
-fi 
-
-if (which node)
-then
-    echo "node: OK"
-else
-    
-    echo "node not found"
-    exit 1
-fi 
-
 if (which g++) && (which cmake) && (which make)
 then
     echo "g++, cmake, make: OK"
@@ -33,13 +15,13 @@ if !(ls /var | grep msr_server)
 then
     sudo mkdir /var/msr_server
 fi
-node_server=$(find . | grep msr_server.ts)
+
 lib=./target/release/libmsr_rs.so
 service1=$(find . | grep msr_server.service)
-service2=$(find . | grep msr_rest_server.service)
-if (sudo cp $service1 /etc/systemd/system/) && (sudo cp $service2 /etc/systemd/system/) && (sudo cp $node_server /var/msr_server/) && (sudo cp $lib /usr/lib/libmsr_rs.so)
+
+if (sudo cp $service1 /etc/systemd/system/) && (sudo cp $lib /usr/lib/libmsr_rs.so)
 then
-    if (sudo cp ./package.json /var/msr_server) && (sudo npm i --prefix /var/msr_server) && (gcc -o msr_gen ./src/main.c -I . -l msr_rs -L /usr/lib/ -lm)
+    if (gcc -o msr_gen ./src/main.c -I . -l msr_rs -L /usr/lib/ -lm)
     then 
         binary=$(find . -depth -maxdepth 2 | grep /msr_gen)
         if (sudo LD_LIBRARY_PATH=/usr/lib/libmsr_rs.so:$LD_LIBRARY_PATH bash -c 'echo $LD_LIBRARY_PATH') && (sudo cp $binary /usr/bin/msr_gen)

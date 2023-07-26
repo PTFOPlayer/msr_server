24834cacf834b757190c472e04ca198f2c70b2a5
PTFOPlayer <patryktofil03@gmail.com>
2023-07-27 00:19:56 +0200

    removed a bounch of unused code

--- src/data_getters/cpuid_data.rs
+++ src/data_getters/cpuid_data.rs
@@ -1,40 +1,8 @@
-use std::ffi::{c_char, CString};
-
 use lazy_static::lazy_static;
 use raw_cpuid::CpuId; 
 
 lazy_static!{
     pub static ref CPUID: CpuId = CpuId::new();
-}
-//static cpuid: CpuId = CpuId::new();
-
-#[no_mangle]
-pub extern "C" fn get_cpu_vendor_rs() -> *const c_char {
-    //let cpuid = CpuId::new();
-    let s = {
-        let this = CPUID.get_vendor_info();
-        match this {
-            Some(val) => val.to_string(),
-            None => "none".to_string(),
-        }
-    }
-    .to_string();
-    let cs = CString::new(s);
-    return cs.unwrap().into_raw();
-}
-
-#[no_mangle]
-pub extern "C" fn get_cpu_name_rs() -> *const c_char {
-    //let cpuid = CpuId::new();
-    let bs = CPUID.get_processor_brand_string();
-    match bs {
-        Some(res) => {
-            let s = res.as_str();
-            let cs = CString::new(s);
-            return cs.unwrap().into_raw();
-        }
-        None => return CString::new("none").unwrap().into_raw(),
-    }
 }
 
 pub fn non_c_name_and_vendor() -> (String, String) {
--- src/data_getters/system_data.rs
+++ src/data_getters/system_data.rs
@@ -1,84 +1,7 @@
-use std::f32::NAN;
-
 use serde::{Deserialize, Serialize};
 use sysinfo::*;
 
 use crate::cpuid_data::CPUID;
-
-/// Deprecated
-#[repr(C)]
-pub struct CoreStat {
-    freq: u64,
-    util: f64,
-    threads: i32,
-    cores: i32,
-    temperature: f32,
-    mem_total: u64,
-    mem_free: u64,
-    mem_used: u64,
-    per_core_freq: *mut u64,
-}
-
-#[no_mangle]
-#[deprecated]
-pub extern "C" fn get_sys_utils_rs(time_mul: i32) -> CoreStat {
-    let mut sys = System::new_all();
-
-    sys.refresh_all();
-
-    std::thread::sleep(std::time::Duration::from_millis(1000 / time_mul as u64));
-
-    sys.refresh_all();
-
-    let t = sys.cpus().len() as i32;
-    let ht = match CPUID.get_feature_info() {
-        Some(val) => val.has_htt(),
-        None => false,
-    };
-
-    let mut c = t;
-    if ht {
-        c = c / 2;
-    }
-
-    let mut s_temp = 0.;
-    let mut i_i = 0;
-    for i in sys.components() {
-        if i.label().contains("coretemp") {
-            s_temp += i.temperature();
-            i_i += 1;
-        }
-    }
-
-    if i_i == 0 {
-        i_i = 1;
-        s_temp = NAN;
-    }
-
-    let mut per_core_freq = sys
-        .cpus()
-        .iter()
-        .map(|c| {
-            c.frequency()
-        })
-        .collect::<Vec<u64>>();
-
-    per_core_freq.shrink_to_fit();
-    let pcf_ptr = per_core_freq.into_boxed_slice().as_mut_ptr();
-
-    let res = CoreStat {
-        freq: sys.global_cpu_info().frequency(),
-        util: sys.global_cpu_info().cpu_usage() as f64,
-        threads: t,
-        cores: c,
-        temperature: s_temp / i_i as f32,
-        mem_total: sys.total_memory(),
-        mem_free: sys.free_memory(),
-        mem_used: sys.used_memory(),
-        per_core_freq: pcf_ptr,
-    };
-    return res;
-}
 
 pub struct CoreStatRs {
     pub freq: u64,
@@ -143,7 +66,7 @@ impl CoreStatRs {
     }
 }
 
-pub fn non_c_sys_utils(time_mul: i32) -> CoreStatRs {
+pub fn sys_utils(time_mul: i32) -> CoreStatRs {
     let mut sys = System::new_all();
 
     sys.refresh_all();

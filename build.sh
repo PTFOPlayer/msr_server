8d8af8554f0bc5002185cd3e877f088d3f5a7c99
PTFOPlayer <patryktofil03@gmail.com>
2023-11-10 18:21:58 +0100

    revriten in rust

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ else
     exit 1
 fi
 
-cargo build --release 
+cargo build --release
 cdir=`pwd`
 
 if !(ls /var | grep msr_server)
@@ -16,27 +16,16 @@ then
     sudo mkdir /var/msr_server
 fi
 
-lib=./target/release/libmsr_rs.so
 service1=$(find . | grep msr_server.service)
 modules=$(find . | grep modules.json)
-if (sudo cp $service1 /etc/systemd/system/) && (sudo cp $lib /usr/lib/libmsr_rs.so) && (sudo cp $modules /var/msr_server)
-then
-    if (gcc -o msr_gen ./src/main.c -I . -l msr_rs -L /usr/lib/ -lm)
-    then 
-        binary=$(find . -depth -maxdepth 2 | grep /msr_gen)
-        if (sudo LD_LIBRARY_PATH=/usr/lib/libmsr_rs.so:$LD_LIBRARY_PATH bash -c 'echo $LD_LIBRARY_PATH') && (sudo cp $binary /usr/bin/msr_gen)
-        then
-            echo "succes, build ended"
-        else
-            echo "error building"
-        fi
-    else
-        echo "error building"
-    fi
-else 
+binary=./target/release/msr_server
+cp $binary .
+mv msr_server msr_gen
+if (sudo cp $service1 /etc/systemd/system/) && (sudo cp $modules /var/msr_server) && (sudo cp ./msr_gen /usr/bin/msr_gen)
+then    
+    echo "succes, build ended"
+else
     echo "error building"
 fi
-
-#build gcc -o main ./src/main.c -I . -l msr_rs -L /usr/lib/ -lm
 
 
--- src/lib.c
+++
@@ -1,3 +0,0 @@
-void print_json_rs();
-void print_toml_rs();
-void server_rs();
